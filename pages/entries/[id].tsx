8bab098f238e767f8bad7ca8f9b6a3692a8e4769
Mauricio Acosta <mauricioacosta@gmail.com>
2022-05-08 19:50:59 -0300

    manejo del formulario

--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import { capitalize, Grid, Card, CardHeader, CardContent, TextField, CardActions, Button, FormControl, FormLabel,FormControlLabel, RadioGroup, Radio, IconButton} from "@mui/material";
 
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
@@ -7,10 +8,51 @@ import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import { Layout } from "../../components/layouts";
 import { EntryStatus } from '../../interfaces';
 
+
 const validStatus: EntryStatus[] =['pending','in-progress','finished']
 
 
 export const EntryPage = () => {
+
+    const [inputValue, setInputValue] = useState('');
+    const [status, setStatus] = useState<EntryStatus>('pending');
+    const [touched, setTouched] = useState(false);
+
+    const onInputValueChanged = (event:ChangeEvent<HTMLInputElement>) => {        
+        setInputValue(event.target.value);
+    }
+
+    const onStatusChanged = (event:ChangeEvent<HTMLInputElement>) =>{
+
+        // setStatus(event.target.value as EntryStatus) 
+        // con esto suplanto el Switch y es lo mismo
+
+        switch (event.target.value) {
+            
+            case 'pending':
+                setStatus('pending') 
+                break;
+            case 'in-progress':
+                setStatus('in-progress') 
+                break;
+            case 'finished':
+                setStatus('finished') 
+                break;
+        
+            default:
+                break;
+        }
+
+ 
+    }
+
+    const onSave = () =>{
+
+        
+    }
+
+
+
   return (
     
     <Layout title=".......">
@@ -23,7 +65,7 @@ export const EntryPage = () => {
             <Grid item xs={12} sm={8} md={6}>
                 <Card>
                     <CardHeader
-                        title="Entrada:"
+                        title={`Entrada: ${inputValue}`}
                         subheader={`Creada hace poco`}
                     
                     />
@@ -36,6 +78,8 @@ export const EntryPage = () => {
                             autoFocus
                             multiline
                             label="Nueva entrada"
+                            value={inputValue}
+                            onChange={onInputValueChanged}
                         
                         />
                         <FormControl>
@@ -44,6 +88,8 @@ export const EntryPage = () => {
                             </FormLabel>
                             <RadioGroup
                                 row
+                                value={status}
+                                onChange={onStatusChanged}
                             >
                                {validStatus.map(option => (
                                    <FormControlLabel
@@ -61,7 +107,8 @@ export const EntryPage = () => {
                         <Button
                             startIcon={<SaveOutlinedIcon />}
                             variant="contained"
-                            fullWidth   
+                            fullWidth
+                            onClick={onSave}   
                         >
                             Save
                         </Button>
